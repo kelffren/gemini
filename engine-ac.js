10a778b93620e6e4747bad7dfefe2e4e77eea972
kelffren <68343904+kelffren@users.noreply.github.com>
2026-08-31 19:34:26 -0400

    V5.13: restore movement - disable NPC push and anti-jitter lock

--- engine-ac.js
+++ engine-ac.js
@@ -23,27 +23,8 @@
     const gait = gaitFrom(mag);
     localPlayer.gait = gait;
     localPlayer._gait = gait;
-    if (gait === 'walk') CONFIG.speed = WALK_SPEED;
-    else if (gait === 'run') CONFIG.speed = RUN_SPEED + (mag - WALK_MAX) * 90;
-    else CONFIG.speed = 0;
+    if (gait === 'run') CONFIG.speed = RUN_SPEED + (mag - WALK_MAX) * 90;
+    else CONFIG.speed = WALK_SPEED;
     _move(dt);
-  };
-
-  const _r = render;
-  render = function () {
-    _r();
-    if (!input.touchActive || isBuildMode) return;
-    const mag = stickMag();
-    ctx.save();
-    ctx.strokeStyle = 'rgba(231,197,106,0.35)';
-    ctx.lineWidth = 2;
-    ctx.beginPath();
-    ctx.arc(input.originX, input.originY, CONFIG.joystickRadius * WALK_MAX, 0, Math.PI * 2);
-    ctx.stroke();
-    ctx.fillStyle = mag < 0.18 ? '#888' : mag < WALK_MAX ? '#e7c56a' : '#39d353';
-    ctx.font = '10px sans-serif';
-    ctx.textAlign = 'center';
-    ctx.fillText(mag < 0.18 ? '' : mag < WALK_MAX ? 'andar' : 'correr', input.originX, input.originY + CONFIG.joystickRadius + 16);
-    ctx.restore();
   };
 })();
