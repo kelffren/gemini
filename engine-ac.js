573a8c6d11cb421a2b2cd3d9c4290478462b0b36
kelffren <68343904+kelffren@users.noreply.github.com>
2026-08-31 21:39:01 -0400

    V5.21 slower run, walk holds most of the stick

--- engine-ac.js
+++ engine-ac.js
@@ -1,7 +1,7 @@
 (function () {
-  const WALK_MAX = 0.62;
-  const WALK_SPEED = 115;
-  const RUN_SPEED = 215;
+  const WALK_MAX = 0.74;
+  const WALK_SPEED = 96;
+  const RUN_SPEED = 165;
   CONFIG.speed = WALK_SPEED;
 
   function stickMag() {
@@ -12,7 +12,7 @@
   }
 
   function gaitFrom(mag) {
-    if (mag < 0.16) return 'idle';
+    if (mag < 0.14) return 'idle';
     if (mag < WALK_MAX) return 'walk';
     return 'run';
   }
@@ -23,7 +23,7 @@
     const gait = gaitFrom(mag);
     localPlayer.gait = gait;
     localPlayer._gait = gait;
-    if (gait === 'run') CONFIG.speed = RUN_SPEED + (mag - WALK_MAX) * 50;
+    if (gait === 'run') CONFIG.speed = RUN_SPEED + (mag - WALK_MAX) * 28;
     else CONFIG.speed = WALK_SPEED;
     _move(dt);
   };
