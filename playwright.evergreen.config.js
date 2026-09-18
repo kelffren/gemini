e137706bdc1b26185231dd8e6a6b6991b5e37d92
kelffren <68343904+kelffren@users.noreply.github.com>
2026-09-18 03:59:46 +0200

    use Playwright iPhone device profile for WebKit gate

--- playwright.evergreen.config.js
+++ playwright.evergreen.config.js
@@ -8,7 +8,7 @@
  * online: local static HTTP server only; real-device Safari remains browserstack.yml
  * do-not: NO UA spoof feature gates, NO production deployment, NO gameplay mutation
  */
-const {defineConfig}=require('@playwright/test');
+const {defineConfig,devices}=require('@playwright/test');
 
 module.exports=defineConfig({
   testDir:'./tests',
@@ -25,13 +25,11 @@ module.exports=defineConfig({
   },
   use:{
     baseURL:'http://127.0.0.1:4173',
+    ...devices['iPhone 14 Pro'],
     browserName:'webkit',
-    viewport:{width:393,height:852},
-    isMobile:true,
-    hasTouch:true,
     trace:'retain-on-failure',
     screenshot:'only-on-failure',
     video:'off'
   },
-  projects:[{name:'webkit-ios-profile',use:{browserName:'webkit'}}]
+  projects:[{name:'webkit-ios-profile',use:{...devices['iPhone 14 Pro'],browserName:'webkit'}}]
 });
